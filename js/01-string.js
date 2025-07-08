2e689286ac1e11f7905d5d9734316791b3cd7302
Angiegoye <goyechea8@gmail.com>
2025-07-08 20:46:39 -0300

    nuevos cambios ahre

--- js/01-string.js
+++ js/01-string.js
@@ -21,3 +21,50 @@ let countries = "Finland, Sweden, Norway, Denmark, and Iceland";
 
 console.log(countries.split(",")); // Dividir en cualquier matriz en coma -> ["Finland", " Sweden", " Norway", " Denmark", " and Iceland"]
 console.log(countries.split(", "));
+
+
+//Contar caracteres incluido el caracter especial (punto,como y espacio)
+
+const nombreUsuario = "angie";
+console.log(nombreUsuario.length);
+
+// Metodo para borrar espacios en los extremos del string
+// .trim()
+
+const nombre ="andres".trim();
+console.log(nombre.length);
+
+//Andres  - ANdres  - ANDRES - andres
+//metodo para transformar cadena de string a minuscula o mayuscula
+
+console.log (nombre.toLowerCase()); // minus
+console.log(nombre.toUpperCase());//mayuscula
+
+//De estatico a dinamico
+
+const usuario = prompt("escribe el nombre del usuario"); // prompt : metodo para crear una ventana emergente
+console.log(usuario);
+
+alert("hola") ; // solo sale un mensaje no deja escribir como un prompt
+
+const usuario1 = prompt("escribe un usuario ").trim();
+const contrasenia = prompt("escribe la contraseña").trim();
+console.log(usuario1)
+
+
+
+const numero1 = prompt("ingresa el primer numero").trim();
+const numero2 = prompt("ingresa el segundo numero").trim();
+const suma = numero1+numero2;
+console.log(suma) // NO suma el prompt devuelve un string ej : num1 :12   num2 :12  suma:1212
+
+
+//ParseInt (transforma en num ) 
+//ParseFloat  Transforma el num a decinal y entero (si le metes una letra lee hasta el numero)
+//Number  Numeros enteros y decimales (si le metes una letra explota)
+
+const numero ="15,15";
+console.log (parseInt(numero)); //15
+console.log(parseFloat(numero));//15,15
+console.log(Number(numero));//15,15
+
